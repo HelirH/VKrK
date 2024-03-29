69c2c6d4bc988ccbc60d992aa32fb6757267df98
HelirH <hoop.helir@gmail.com>
2024-03-29 19:00:55 +0200

    custom 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<h1>404</h1>
+<p>Lehte ei leitud</p>
+<p>See demo sisaldab vaid neid kodulehe osi, mis kõige rohkem täiustamist vajasid. Seetõttu viivad enamus linke siia lehele.</p>
+
+<style>
+    h1 {
+        display: block;
+        font-size: 2em;
+        margin-block-start: 0.67em;
+        margin-block-end: 0.67em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        font-weight: bold;
+        unicode-bidi: isolate;
+    }
+
+    p {
+        display: block;
+        margin-block-start: 1em;
+        margin-block-end: 1em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        unicode-bidi: isolate;
+    }
+</style>
